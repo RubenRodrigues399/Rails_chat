5cdbb5c1e8184e3eed81fbb7cac5377924254ef3
RubenRodrigues399 <rubenzidane08@gmail.com>
2025-07-21 20:28:00 +0100

    Actualizando o controller das mensagens, o model mensagens e o model users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_17_152901) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_21_185526) do
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,7 +27,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_17_152901) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
+
+  add_foreign_key "messages", "users"
 end
