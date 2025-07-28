478e4e7490d293867b8cb5a37ba449f55a7869ac
RubenRodrigues399 <rubenzidane08@gmail.com>
2025-07-28 22:14:23 +0100

    update messages_controler

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,29 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
-    before_action :set_message, only: [:destroy]
+  before_action :set_message, only: [:destroy]
 
-    def create
-        message = current_user.messages.build(message_params)
-        message.save
+  def create
+    @message = current_user.messages.build(message_params)
+
+    if @message.save
+      respond_to do |format|
+        format.turbo_stream # responde com turbo_stream format
+        format.html { redirect_to root_path } # fallback
+      end
     end
+  end
 
-    def destroy
-        @message.destroy
-    end
+  def destroy
+    @message.destroy
+  end
 
-    private
+  private
 
-    def message_params
-        params.require(:message).permit(:body)
-    end
+  def message_params
+    params.require(:message).permit(:body)
+  end
 
-    def set_message
-        @message = Message.find(params[:id])
-    end
+  def set_message
+    @message = Message.find(params[:id])
+  end
 end
