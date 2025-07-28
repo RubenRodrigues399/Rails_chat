ba33b467e185d2a7922aea7ea01e402184828c33
RubenRodrigues399 <rubenzidane08@gmail.com>
2025-07-28 22:09:36 +0100

    Update javascript.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import ResetFormController from "./controllers/reset_form_controller"
+application.register("reset-form", ResetFormController)
+
